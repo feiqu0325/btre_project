939f0b40d5ca4b090b432e78fc492072fd3661b6
Fei Qu <feiqu@Feis-MacBook-Air.local>
2021-11-13 17:48:17 -0800

    Initial commit

---
+++ listings/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.db import models
+from .models import Listing
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
+admin.site.register(Listing, ListAdmin)
